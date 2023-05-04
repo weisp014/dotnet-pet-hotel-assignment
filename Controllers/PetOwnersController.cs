af426c593d15915e3a49c3c87167c07495193100
Ryan Weispfenning <weisp014@gmail.com>
2023-05-04 13:59:07 -0500

    fixed GET and POST requests to send correct status and data

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -12,23 +12,27 @@ namespace pet_hotel.Controllers
     public class PetOwnersController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetOwnersController(ApplicationContext context) {
+        public PetOwnersController(ApplicationContext context)
+        {
             _context = context;
         }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<PetOwner> GetPets() {
+        public IEnumerable<PetOwner> GetPets()
+        {
             // return new List<PetOwner>();
             return _context.PetOwners;
         }
 
         [HttpGet("{id}")]
-        public ActionResult<PetOwner> Get(int id) {
+        public ActionResult<PetOwner> Get(int id)
+        {
             PetOwner petOwner = _context.PetOwners.SingleOrDefault(p => p.id == id);
 
-            if (id != petOwner.id) {
+            if (id != petOwner.id)
+            {
                 return BadRequest();
             }
 
@@ -36,30 +40,34 @@ namespace pet_hotel.Controllers
         }
 
         [HttpPost]
-        public PetOwner Post (PetOwner petOwner) {
+        public IActionResult Create(PetOwner petOwner)
+        {
+            _context.Add(petOwner);
+            _context.SaveChanges();
 
-        _context.Add(petOwner);
-        _context.SaveChanges();
-
-        return petOwner;
+            return CreatedAtAction(nameof(Get), new { id = petOwner.id }, petOwner);
         }
 
         [HttpPut("{id}")]
-        public IActionResult Put(int id, PetOwner petOwner) {
-            if (id != petOwner.id) {
+        public IActionResult Put(int id, PetOwner petOwner)
+        {
+            if (id != petOwner.id)
+            {
                 return BadRequest();
             }
 
             _context.Update(petOwner);
             _context.SaveChanges();
-            return NoContent();
+            return Ok(petOwner);
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id) {
+        public IActionResult Delete(int id)
+        {
             PetOwner petOwner = _context.PetOwners.SingleOrDefault(p => p.id == id);
 
-            if(petOwner is null) {
+            if (petOwner is null)
+            {
                 return NotFound();
             }
 
